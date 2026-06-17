693791e275cda035db18d536040acc418b3df2ae
jand05x <jandismat@yahoo.com>
2026-06-18 01:59:58 +0300

    Fix: keep public/ dir for Vite manifest, copy boot files to root instead of flattening

--- public/setup.php
+++ public/setup.php
@@ -1,8 +1,8 @@
 <?php
 
 // Bootstrap Laravel
-require __DIR__ . '/vendor/autoload.php';
-$app = require __DIR__ . '/bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
